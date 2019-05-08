3a9132e29453642b4df7321b55300d52fe59f2b3
deicidem <kronosdidi@gmail.com>
2019-05-08 22:42:41 +0300

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 $(document).ready(() => {
-  let popup = $('.popup'),
-    overlay = $('.overlay');
+  $('.lines').css('height', $(window).height() - $('.footer').height() - 36);
   $('.popup-open').on('click', () => {
     $('.popup, .overlay').fadeIn();
-    $('body').css('overflow', 'hidden')
+    $('body').css('overflow', 'hidden');
   });
 
   $('.popup-close').on('click', () => {
     $('.popup, .overlay').fadeOut();
-    $('body').css('overflow', 'auto')
+    $('body').css('overflow', 'auto');
   });
+
+  $(window).resize(function () {
+    $('.lines').css('height', $(window).height() - $('.footer').height() - 36);
+  });
+
+  $("#phone").mask("+7 (999) 999-99-99");
+
 });
