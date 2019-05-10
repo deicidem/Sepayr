b5fc7ee3003927235b4231d7a88b742f8e5391c2
deicidem <kronosdidi@gmail.com>
2019-05-10 14:28:27 +0300

    a

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
   $('.lines').css('height', $('body').height() - $('.footer').height() - 36);
   $('.popup-open').on('click', () => {
-    $('.popup, .overlay').fadeIn();
+    $('.popup').fadeOut();
+    $('.popup-form, .overlay').fadeIn();
     $('body').css('overflow', 'hidden');
   });
 
@@ -9,6 +10,13 @@ $(document).ready(() => {
     $('.popup, .overlay').fadeOut();
     $('body').css('overflow', 'auto');
   });
+
+  $('.popup-agree, .footer-copyright a').on('click', () => {
+    $('.popup').fadeOut();
+    $('.popup-privacy, .overlay').fadeIn();
+    $('body').css('overflow', 'hidden');
+  });
+
 
   $(window).resize(function () {
     $('.lines').css('height', $('body').height() - $('.footer').height() - 36);
